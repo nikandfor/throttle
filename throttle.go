a8503beb8b0b806feccfce0a595fe89742f953d6
nikandfor <nikandfor@gmail.com>
2023-11-25 00:28:26 +0100

    update

--- throttle.go
+++ throttle.go
@@ -8,10 +8,23 @@ import (
 type (
 	Throttle struct {
 		lastts int64
-		price  int64
-		limit  int64
+		Price  int64
+		Limit  int64
 	}
 )
+
+func NewRateLimit(value, rate, limit float64) *Throttle {
+	ts := time.Now().UnixNano()
+	price := int64(1e9 / rate)
+	lim := int64(1e9 * rate * limit)
+	tokens := ts - int64(1e9/rate*value)
+
+	return &Throttle{
+		lastts: ts - tokens,
+		Price:  price,
+		Limit:  lim,
+	}
+}
 
 func NewRateWindow(value, rate float64, window time.Duration) *Throttle {
 	ts := time.Now().UnixNano()
@@ -21,23 +34,37 @@ func NewRateWindow(value, rate float64, window time.Duration) *Throttle {
 
 	return &Throttle{
 		lastts: ts - tokens,
-		price:  price,
-		limit:  limit,
+		Price:  price,
+		Limit:  limit,
 	}
 }
 
 func New(ts, price, limit int64) *Throttle {
 	return &Throttle{
 		lastts: ts,
-		price:  price,
-		limit:  limit,
+		Price:  price,
+		Limit:  limit,
 	}
 }
 
 func (t *Throttle) Reset(ts, price, limit int64) {
 	t.lastts = ts
-	t.price = price
-	t.limit = limit
+	t.Price = price
+	t.Limit = limit
+}
+
+func (t *Throttle) Value(now time.Time) int {
+	return t.ValueTs(now.UnixNano())
+}
+
+func (t *Throttle) ValueTs(ts int64) int {
+	return int(t.ValueTsCost(ts) / t.Price)
+}
+
+func (t *Throttle) ValueTsCost(ts int64) int64 {
+	t.advance(ts)
+
+	return ts - t.lastts
 }
 
 func (t *Throttle) Have(now time.Time, n int) bool {
@@ -45,7 +72,7 @@ func (t *Throttle) Have(now time.Time, n int) bool {
 }
 
 func (t *Throttle) HaveTs(ts int64, n int) bool {
-	return t.HaveTsCost(ts, t.price*int64(n))
+	return t.HaveTsCost(ts, t.Price*int64(n))
 }
 
 func (t *Throttle) HaveTsCost(ts, cost int64) bool {
@@ -59,7 +86,7 @@ func (t *Throttle) Take(now time.Time, n int) bool {
 }
 
 func (t *Throttle) TakeTs(ts int64, n int) bool {
-	return t.TakeTsCost(ts, t.price*int64(n))
+	return t.TakeTsCost(ts, t.Price*int64(n))
 }
 
 func (t *Throttle) TakeTsCost(ts, cost int64) bool {
@@ -77,7 +104,7 @@ func (t *Throttle) Borrow(now time.Time, n int) time.Duration {
 }
 
 func (t *Throttle) BorrowTs(ts int64, n int) time.Duration {
-	return t.BorrowTsCost(ts, t.price*int64(n))
+	return t.BorrowTsCost(ts, t.Price*int64(n))
 }
 
 func (t *Throttle) BorrowTsCost(ts, cost int64) time.Duration {
@@ -97,7 +124,7 @@ func (t *Throttle) Wait(ctx context.Context, now time.Time, n int) error {
 }
 
 func (t *Throttle) WaitTs(ctx context.Context, ts int64, n int) error {
-	return t.WaitTsCost(ctx, ts, t.price*int64(n))
+	return t.WaitTsCost(ctx, ts, t.Price*int64(n))
 }
 
 func (t *Throttle) WaitTsCost(ctx context.Context, ts, cost int64) error {
@@ -118,11 +145,35 @@ func (t *Throttle) WaitTsCost(ctx context.Context, ts, cost int64) error {
 	return nil
 }
 
+func (t *Throttle) SetValue(now time.Time, n int) {
+	t.SetValueTs(now.UnixNano(), n)
+}
+
+func (t *Throttle) SetValueTs(ts int64, n int) {
+	t.SetValueTsCost(ts, t.Price*int64(n))
+}
+
+func (t *Throttle) SetValueTsCost(ts, cost int64) {
+	t.lastts = ts - cost
+}
+
+func (t *Throttle) Return(now time.Time, n int) {
+	t.ReturnTs(now.UnixNano(), n)
+}
+
+func (t *Throttle) ReturnTs(ts int64, n int) {
+	t.ReturnTsCost(ts, t.Price*int64(n))
+}
+
+func (t *Throttle) ReturnTsCost(ts, cost int64) {
+	t.spend(-cost)
+}
+
 func (t *Throttle) advance(ts int64) {
 	tokens := ts - t.lastts
 
-	if tokens > t.limit {
-		tokens = t.limit
+	if tokens > t.Limit {
+		tokens = t.Limit
 	}
 
 	t.lastts = ts - tokens
